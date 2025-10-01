026d325cfc4ed547b029da80431e01b6309afb89
Jason <jay.bourner@googlemail.com>
2025-10-01 20:51:58 +0000

    fixed the links on contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,9 +3,9 @@
         <h1>Contact me for info</h1>
         <div class="contact-methods">
             <div v-for="method in contact.methods" :key="method.label" class="method-item">
-                <button :href="method.link" target="_blank" rel="noopener">
+                <a :href="method.link" target="_blank" rel="noopener">
                     <img :src="method.icon" alt="">
-                </button>
+                </a>
                 <span>{{ method.value }}</span>
             </div>
         </div>
@@ -34,7 +34,6 @@ export default {
 .contact {
     display: flex;
     width: 75%;
-    // height: 75%;
     gap: 40px;
     padding: 50px;
     align-items: center;
@@ -65,30 +64,21 @@ export default {
         grid-template-columns: 10% auto;
 
         span {
-            // padding-left: 20px;
             text-align: left;
         }
 
-        button {
-            // all: unset;
+        a {
             cursor: pointer;
             width: 60px;
             height: 40px;
-            // display: flex;
-            // align-items: center;
-            // justify-content: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             border-radius: 10px;
             background-color: #ffc18e;
+            border: 2px solid #666;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            // transition: background-color 0.3s ease, transform 0.2s ease;
         }
-
-        // img {
-        //     width: 100%;
-        //     height: 100%;
-        //     object-fit: cover;
-        //     border-radius: 10px;
-        // }
         
         @include breakpoint(md) {
             grid-template-columns: 15% auto;
@@ -98,32 +88,5 @@ export default {
             grid-template-columns: auto;
         }
     }
-
-    // a {
-    //     text-decoration: none;
-    //     font-size: 1.5rem;
-    //     font-weight: bold;
-    //     color: #333;
-    //     transition: all 0.3s ease;
-    // }
-
-    // a:hover {
-    //     color: #007BFF;
-    //     transform: scale(1.1);
-    // }
-
-    // a:active {
-    //     color: #0056b3;
-    //     transform: scale(0.9);
-    // }
-
-    // span {
-    //     font-size: 1.2rem;
-    //     font-weight: bold;
-    //     text-transform: uppercase;
-    //     letter-spacing: 2px;
-    //     color: #333;
-    //     transition: all 0.3s ease;
-    // }
 }
 </style>
